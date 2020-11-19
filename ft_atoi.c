8e35cfdd9ef43f88bd710272371f916b11431118
dhers <dhers@student.fr>
2020-11-19 16:45:12 +0100

    first try

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,21 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dhers <dhers@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/19 15:47:40 by dhers             #+#    #+#             */
-/*   Updated: 2020/11/19 16:16:30 by dhers            ###   ########.fr       */
+/*   Created: 2020/11/19 16:14:14 by dhers             #+#    #+#             */
+/*   Updated: 2020/11/19 16:34:43 by dhers            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-size_t	ft_strlen(const char *s)
+int	ft_atoi(const char *nptr)
 {
-	size_t i;
+	int i;
+	int sign;
+	int result;
 
 	i = 0;
-	while (s[i] != '\0')
+	sign = 1;
+	result = 0;
+	while ((nptr[i] >= 11 && nptr[i] <= 15) || nptr[i] == 32)
 		i++;
-	return (i);
+	if (nptr[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (nptr[i] == '+')
+		i++;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - 48);
+		i++;
+	}
+	return (result * sign);
 }
